ed58082dc456827ecd3b420b4f1afd88466230c6
Nedrath <Mark.P.Eriksen@gmail.com>
2021-03-05 15:16:01 +0100

    card done

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import { css } from "@emotion/css";
+import BreakfastCard from "../components/BreakfastCard";
 import RecipeOfTheDay from "../components/RecipeOfTheDay";
 
 const Home = () => {
@@ -9,12 +10,40 @@ const Home = () => {
     height: 100%;
     /* grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); */
   `;
+  const style_Header = css`
+    margin: auto;
+    padding: 0 15px;
+  `;
+  const style_Main = css`
+    margin: auto;
+    padding: 0 15px;
+  `;
+  const style_Headline = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 25px;
+    padding-bottom: 10px;
+    h2 {
+      font-size: 1.5em;
+      font-weight: 600;
+    }
+    p {
+      color: orange;
+    }
+  `;
   return (
     <div className={style_Grid}>
-      <header>
+      <header className={style_Header}>
         <RecipeOfTheDay />
       </header>
-      <main></main>
+      <main className={style_Main}>
+        <div className={style_Headline}>
+          <h2>Breakfast</h2>
+          <p>--></p>
+        </div>
+        <BreakfastCard />
+      </main>
       <footer></footer>
     </div>
   );
