0117c01db5e568d61fa7ef5714dc6a44e3dde3e2
Nedrath <Mark.P.Eriksen@gmail.com>
2021-03-09 10:37:34 +0100

    header delen done

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,21 @@ const Header = () => {
     position: absolute;
     top: 0;
   `;
+  const style_PlayIcon = css`
+    position: absolute;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 100px;
+    width: 100%;
+    height: 100%;
+    font-size: 5em;
+    ion-icon {
+      color: white;
+      opacity: 0.4;
+    }
+  `;
   const style_Hero = css`
     object-fit: cover;
     opacity: 0.4;
@@ -41,12 +56,28 @@ const Header = () => {
     bottom: 4%;
     width: 50%;
     padding-left: 25px;
-    padding-bottom: 75px;
+    padding-bottom: 45px;
+  `;
+  const style_NameBox = css`
+    padding: 25px 25px;
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  `;
+  const style_Follow = css`
+    color: orange;
+    font-weight: 400;
+    font-size: 0.8em;
   `;
 
   return (
     <div className={style_Header}>
       <img className={style_Hero} src={RecipeHeroImg} alt="food" />
+      <div className={style_PlayIcon}>
+        <ion-icon name="play-circle-outline"></ion-icon>
+      </div>
       <div className={style_IconsBox}>
         <Link to="/home">
           <ion-icon name="arrow-back"></ion-icon>
@@ -54,7 +85,30 @@ const Header = () => {
         <ion-icon name="heart-outline"></ion-icon>
       </div>
       <h1 className={style_Headline}>Swedish Meatballs</h1>
-      {/* </div> */}
+      <section className={style_NameBox}>
+        <p
+          className={css`
+            color: white;
+            font-weight: 400;
+            font-size: 0.8em;
+            display: flex;
+          `}
+        >
+          By
+          <span>
+            <p
+              className={css`
+                color: orange;
+                padding-left: 5px;
+              `}
+            >
+              Michael Bezot
+            </p>
+          </span>
+        </p>
+
+        <p className={style_Follow}>+Follow</p>
+      </section>
     </div>
   );
 };
